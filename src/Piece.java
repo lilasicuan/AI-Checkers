669db40b12c3280f7dfe644c3d3c03899e428b6a
Viktoria Lila Sicuan <viktoria_sicuan@dlsu.edu.ph>
2022-01-03 14:14:31 +0800

    Added row and column attributes to Piece objects

--- src/Piece.java
+++ src/Piece.java
@@ -1,10 +1,30 @@
 public class Piece {
     private boolean maxPiece;
     private boolean kingPiece;
+    private int row;
+    private int col;
 
-    public Piece(boolean max) {
+    public Piece(boolean max, int row, int col) {
+        this.row = row;
+        this.col = col;
         maxPiece = max;
         kingPiece = false;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public void setCol(int col) {
+        this.col = col;
     }
 
     public void setKing() {
